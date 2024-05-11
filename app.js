5daa27c6b0cdec72be0461a351711e7a264e9b6b
Pablo Cantos <pablocantos051099@gmail.com>
2024-05-11 12:26:36 -0300

    Instale cors que nos ayuda con las peticiones HTTPS y agregue comentarios a los archivos

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ const express = require('express');
 const dbConnection = require('./database/config');
 require('dotenv').config();
 
-dbConnection();
+dbConnection(); //Conexion con la base de datos
 const app = express();
+const cors = require('cors');
 
 app.listen(process.env.PORT, () => {
-	console.log(`Ejecutandose en el puerto ${process.env.PORT}`);
+	console.log(`Ejecutandose en el puerto ${process.env.PORT}`); //Levantamos nuestro servidor
 });
 
-app.use('/admin', require('./router/adminRouter'));
+app.use(cors()); //Ayuda para las peticiones HTTPS
 
-app.use('/reservas', require('./router/reservasRouter'));
+app.use('/admin', require('./router/adminRouter')); //Rutas para el admin
+
+app.use('/reservas', require('./router/reservasRouter')); //Rutas para las reservas
