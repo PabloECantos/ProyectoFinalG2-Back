6856becb8ab6ad6ff849ec6893707533d618c47e
Pablo Cantos <pablocantos051099@gmail.com>
2024-05-12 15:15:41 -0300

    Agregue una peticion get para mandar los usuarios registrados

--- controllers/adminControllers.js
+++ controllers/adminControllers.js
@@ -1,10 +1,14 @@
 // Aqui manejaremos las autenticaciones de la adminPage
 
+const sendUsuarios = async (req, res) => {
+	try {
+		const listUsers = await Usuarios.find();
 
-const sendUsuarios = (req, res) => {
-	res.send('Usuarios mandados');
+		res.status(200).json({
+			listUsers,
+		});
+	} catch (error) {}
 };
-
 
 module.exports = {
 	sendUsuarios,
