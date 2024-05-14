40ca00aa43adb959de5f0ff8d7d89c4da995badf
Franco Tarulli <franco.tarulli@outlook.es>
2024-05-14 18:23:43 -0300

    Configure ruta reservas

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 const express = require('express');
+const dbConnection = require('./database/config');
+const app = express();
+require('dotenv').config();
+
+app.use(express.json());
+
+app.use('/auth', require('./router/reservasRouter'));
+
+dbConnection();
+
+app.listen(process.env.PORT, () => {
+	console.log(`ejecutandose en el puertos ${process.env.PORT}`);
+});
