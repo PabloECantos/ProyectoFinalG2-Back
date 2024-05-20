79276b515f8ab6312ef5b777dcadee70647155db
Vanina <vaniibalbastro@gmail.com>
2024-05-20 17:59:10 -0300

    terminado login y registro backend

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
 const express = require('express');
-
 const app = express();
+const cors = require('cors');
+const dbConnection = require('./database/config');
+require('dotenv').config();
+
+app.use(express.json());
+
+// app.use('/auth', require('./router/reservasRouter'));
+
+app.use(cors()); //Ayuda para las peticiones HTTPS
+app.use('/user', require('./router/userRouter'))
+
+// app.use('/admin', require('./router/adminRouter')); //Rutas para el admin
+
+// app.use('/reservas', require('./router/reservasRouter')); //Rutas para las reservas
+
+dbConnection(); //Conexion con la base de datos
+
+app.listen(process.env.PORT, () => {
+	console.log(`Ejecutandose en el puerto ${process.env.PORT}`); //Levantamos nuestro servidor
+});
+
