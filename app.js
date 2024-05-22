42dcff6f012c73da71b78d164df190b4fa8cf5c2
Franco Tarulli <franco.tarulli@outlook.es>
2024-05-22 12:25:33 -0300

    cors

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const dbConnection = require('./database/config');
+const cors = require('cors');
 const app = express();
 require('dotenv').config();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use('/auth', require('./router/reservasRouter'));
 
