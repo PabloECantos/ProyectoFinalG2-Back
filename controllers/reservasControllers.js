458698b1ed68fea5853831c659e881c3c408ebe3
Franco Tarulli <franco.tarulli@outlook.es>
2024-05-22 12:28:03 -0300

    Actualice logica para comprobar si hay mesas disponibles

--- controllers/reservasControllers.js
+++ controllers/reservasControllers.js
@@ -1,64 +1,56 @@
 //Aqui manejaremos las autenticaciones de la  pagina de reservas
-const fechaHoraActual = new Date();
-const options = {
-	timeZone: 'America/Argentina/Buenos_Aires',
-	weekday: 'long',
-	hour: 'numeric',
-	minute: 'numeric',
-	day: 'numeric',
-	hour12: false,
+const e = require('cors');
+const Reservas = require('../models/ReservasModel');
+
+const sendReservas = async (req, res) => {
+	const { time, date } = req.body;
+	const tiempoIngresado = new Date(time);
+	const registros = await Reservas.find({});
+	for (let i = 0; i < registros.length; i++) {
+		console.log('Registro', i + 1, 'time:', registros[i].time);
+
+		console.log(
+			`TiempoIngresado ${tiempoIngresado} , tiempo en for ${registros[i].time}`
+		);
+		if (registros.length === 0) {
+			return true;
+		} else if (tiempoIngresado === registros[i].time) {
+			console.log('Se encontraron registros con el mismo valor de time:', registros);
+			return false;
+		} else {
+			console.log('No se encontraron registros');
+			return true;
+		}
+	}
+	console.log(registros.length);
 };
-const fechaHoraArgentina = fechaHoraActual.toLocaleString('es-AR', options);
-
-console.log('first');
-
-const reservamesa = require('../models/ReservasModel');
 
 const saveReservas = async (req, res) => {
-	const { name, quantity, date } = req.body;
+	const { name, time, cant, date, phone, email, comment } = req.body;
 
-	if (!date || !name || !quantity) {
+	const compLugar = await sendReservas(req);
+
+	if (!name || !time || !cant) {
 		return res.status(400).json({
-			msg: 'Todos los campos son obligatorios',
+			msg: 'Por favor, complete los campos obligatorios',
 		});
 	}
 
 	try {
-		//comprobamos si hay reservas disponibles
-		let r = await reservamesa.find({ date });
-		if (r) {
+		if (compLugar === true) {
+			let reserva = new Reservas(req.body);
+
+			await reserva.save();
+			return res.status(201).json({
+				msg: 'Reserva Registrada',
+			});
+		} else {
 			return res.status(400).json({
-				msg: 'Ya esta registrada esta reserva',
+				msg: 'El horario que deseas no se encuentra disponible!',
 			});
 		}
-
-		//creamos instancia si no encuentra horario
-		r = new reservamesa(req.body);
-
-		// await r.save();
-
-		res.status(201).json({
-			msg: 'Funciona',
-		});
-		//Creamos la instancia
-
-		// if ((fechaHoraActual) => registrarReserva.date) {
-		// 	console.log('Hora incorrecta, elige otro horario');
-
-		// 	console.log(fechaHoraActual);
-		// 	console.log(registrarReserva);
-		// 	res.status(500).json({
-		// 		msg: 'Se ha producido un error',
-		// 	});
-		// } else {
-		// 	registrarReserva = new ReservasClientes(req.body);
-		// 	//guardamos en base de datos
-		// 	await registrarReserva.save();
-		// 	res.status(201).json({
-		// 		msg: 'Reserva Registrada',
-		// 	});
-		// }
 	} catch (error) {
+		console.log(error);
 		res.status(500).json({
 			msg: 'Se ha producido un error',
 		});
@@ -67,4 +59,5 @@ const saveReservas = async (req, res) => {
 
 module.exports = {
 	saveReservas,
+	sendReservas,
 };
