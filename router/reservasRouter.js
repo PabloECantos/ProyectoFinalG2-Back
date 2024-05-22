a008a23b44cd8cb023003bd4237ce8ad4373274d
Franco Tarulli <franco.tarulli@outlook.es>
2024-05-22 12:26:19 -0300

    Agrege peticion get para imprimir la base de datos

--- router/reservasRouter.js
+++ router/reservasRouter.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { saveReservas } = require('../controllers/reservasControllers');
+const { saveReservas, sendReservas } = require('../controllers/reservasControllers');
 const reservasRouter = express.Router();
 
 //Aqui manejamos todas las peticiones de las reservas
 
 reservasRouter.post('/saveReservas', saveReservas); //Peticion post para guardar las reservas en la base de datos
+
+reservasRouter.get('/sendReservas', sendReservas); //
 
 module.exports = reservasRouter;
