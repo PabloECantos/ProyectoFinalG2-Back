253bc4d67f442e30809f05d0b773e2e181bf9180
Pablo Cantos <pablocantos051099@gmail.com>
2024-06-04 20:30:47 -0300

    Volvi a modificar el adminrouter

--- router/adminRouter.js
+++ router/adminRouter.js
@@ -4,7 +4,6 @@ const {
 	sendReservas,
 	deleteUsers,
 	deleteReservas,
-	editUsers,
 	editReservas,
 } = require('../controllers/adminControllers');
 const validarJWT = require('../middleware/JWTValidator');
@@ -17,9 +16,9 @@ AdminRouter.get('/sendUsers', validarJWT, sendUsuarios); //Peticion get para env
 
 AdminRouter.get('/sendReservas', validarJWT, sendReservas); //Peticion get para enviar las reservas al front
 
-AdminRouter.delete('/deleteUsers/:_id', validarJWT, deleteUsers); // Peticion delete para eliminar usuarios de la base de datos
+AdminRouter.delete('/deleteUsers/:id', validarJWT, deleteUsers); // Peticion delete para eliminar usuarios de la base de datos
 
-AdminRouter.delete('/deleteReservas/:_id', validarJWT, deleteReservas); // Peticion delete para eliminar reservas de la base de datos
+AdminRouter.delete('/deleteReservas/:id', validarJWT, deleteReservas); // Peticion delete para eliminar reservas de la base de datos
 
 AdminRouter.put('/editReservas', validarJWT, editReservas); // Peticion put para editar datos de las reservas en la base de datos
 
